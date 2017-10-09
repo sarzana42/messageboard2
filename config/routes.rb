999a942bae404d7f269b6c5de89b8eb2c80085eb
sarzana42 <sarzana42@gmail.com>
2017-10-09 04:54:43 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'messages/index'
   root 'messages#index'
-
+  resources :messages , only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
